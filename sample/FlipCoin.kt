288953a74b9b3fdaa83650105aaf8803a9cff815
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-30 16:27:03 +0530

    [soundarya]Refactor. cleared warnings

--- sample/FlipCoin.kt
+++ sample/FlipCoin.kt
@@ -12,12 +12,12 @@ package com.bridgelabs.sample
  * Function to flip a coin trails number of times
  * and return heads vs tails.
  *
- * @param tails : Number of times to flip a coin.
+ * @param trails : Number of times to flip a coin.
  * @return heads/tails.
  */
 fun flipCoin(trails: Int): Double {
-    var heads: Int = 0
-    var tails: Int = 0
+    var heads = 0
+    var tails = 0
     for (index in 0.rangeTo(trails)) {
         if (Math.random() > 0.5) heads++
         tails++
@@ -28,12 +28,9 @@ fun flipCoin(trails: Int): Double {
 /**
  * Main function to print
  * number of heads vs number of tails.
- *
- * @param tails : Takes in number of times to flip a coin.
- * @param headvstail : number of heads / number of tails.
  */
 fun main() {
-    val tails = 100
-    val headvstail = flipCoin(tails)
+    val trails = 100
+    val headvstail = flipCoin(trails)
     println("Head v/s Tails are $headvstail")
 }
