95f369f233f7aa74406e36b4577eabbf768b05af
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-30 12:31:05 +0530

    [soundarya]Refactor. Added file Comment

--- sample/HelloWorld.kt
+++ sample/HelloWorld.kt
@@ -1,12 +1,20 @@
 package com.bridgelabs.sample
 
+/***************************
+ * @purpose To print Hello World message with user name and user age.
+ * @file HelloWorld.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 28/12/2019
+ ***************************
+ **************************/
 /**
  * Function to print Hello World with userName
  * and user age
  */
 fun main() {
     val userName = "Soundarya"
-    var age = 22
+    val age = 22
     println("Hello World. My name is $userName. I am $age years old")
 
 }
