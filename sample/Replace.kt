be8ecffe32606599d200e2392c1a4276894f051b
ksoundarya4 <ksoundarya4@gmail.com>
2019-12-30 12:36:20 +0530

    [soundarya]Refactor. Added file comments

--- sample/Replace.kt
+++ sample/Replace.kt
@@ -1,10 +1,18 @@
+/***********************************
+ * @purpose To replace "<<UserName>>" with user name and print the result
+ * @file Replace.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 28/12/2019
+ ***********************************
+ **********************************/
 package com.bridgelabs.sample
 
-val templet : String = "Hello <<UserName>> , How are you?"
+val templet: String = "Hello <<UserName>> , How are you?"
 var name = "Soundarya"
 fun main() {
 
-    val result = "${templet.replace("<<UserName>>" , name)}"
-        println(result)
-    }
+    val result = "${templet.replace("<<UserName>>", name)}"
+    println(result)
+}
 
