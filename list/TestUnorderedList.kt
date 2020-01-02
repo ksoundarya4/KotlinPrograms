4ad0961c2da5aa2bbcb223133bf50d2fe05a7863
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-02 12:14:52 +0530

    [soundarya]Add. added TestUnorderedList.kt

--- list/TestUnorderedList.kt
+++ list/TestUnorderedList.kt
@@ -1,11 +1,16 @@
-package com.bridgelabs.sample
+package com.bridgelabs.list
 
 fun main(){
     val list = UnorderedList<Int>()
+    println(list.size)
     list.add(2)
+    println(list.size)
     list.add(4)
+    println(list.size)
     println(list)
     list.add(3)
     list.add(1)
     println(list)
+    println(list.size)
+
 }
