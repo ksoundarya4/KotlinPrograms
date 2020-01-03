d9b80534f553c54cfb6304dfb46742e06f94c7d5
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-03 16:06:16 +0530

    [soundarya]Refactor. Added pop function to Stack.kt

--- stack/Stack.kt
+++ stack/Stack.kt
@@ -7,24 +7,33 @@ data class Stack<T : Comparable<T>>(
     var size: Int = 0
 ) {
     override fun toString(): String {
-       var str = ""
-        var temp : Node<T> ?= top
+        var str = ""
+        var temp: Node<T>? = top
 
-        while(temp != null){
-            if(temp.next != null)
+        while (temp != null) {
+            if (temp.next != null)
                 str += temp.data.toString() + ","
-            if(temp.next == null)
+            if (temp.next == null)
                 str += temp.data
             temp = temp.next
         }
         return "[$str]"
     }
 
-    fun push(data: T){
+    fun push(data: T) {
 
         val newNode = Node(data)
         newNode.next = top
         top = newNode
         size++
+    }
+
+    fun pop(): T {
+        if (size == 0) throw StackIsEmptyException(" Stack is Empty ")
+
+        val oldNode = top!!
+        top = oldNode.next
+        size--
+        return oldNode.data!!
     }
 }
