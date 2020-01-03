22523a9e556b47e6656b1a9cab89789ee51cff2e
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-03 17:37:58 +0530

    [soundarya]Refactor. Added file comment

--- stack/StackIsEmptyException.kt
+++ stack/StackIsEmptyException.kt
@@ -1,3 +1,11 @@
+/**
+ * Custom Exception
+ * @purpose To raise an exception when an Stack is Empty.
+ * @file StackIsEmptyException.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 03/01/2020
+ */
 package com.bridgelabs.stack
 
 class StackIsEmptyException(message: String) : Throwable(message)
