8eb65dc96dd21af2b9ae230a56399aa313e8c999
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-03 18:35:32 +0530

    [soundarya]Refactor. Added toString and enqueue functions to Queque.kt

--- queue/Queue.kt
+++ queue/Queue.kt
@@ -5,7 +5,35 @@ import com.bridgelabs.list.Node
 data class Queue<T>(
     private var head: Node<T>? = null,
     private var tail: Node<T>? = null,
-    private var size: Int = 0
+    var size: Int = 0
 ) {
+    override fun toString(): String {
+        var string = ""
+        var temp = head
+        while (temp != null) {
+            if (temp.next == null) {
+                string += temp.data.toString()
+            }
+            if (temp.next != null) {
+                string += temp.data.toString() + ","
+            }
+            temp = temp.next
+        }
+        return "[$string]"
+    }
 
+    fun enequeue(data: T) {
+        var newNode = Node<T>(data)
+
+        if (head == null && tail == null) {
+            head = newNode
+            tail = newNode
+        }
+
+        if (head != null) {
+            tail?.next = newNode
+            this.tail = newNode
+        }
+        size++
+    }
 }
