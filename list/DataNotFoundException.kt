c94602fe109d704050cc01c536a6aec3fe63ffd1
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-03 17:36:00 +0530

    [soundarya]Refactor. Added file comment

--- list/DataNotFoundException.kt
+++ list/DataNotFoundException.kt
@@ -1,3 +1,11 @@
+/**
+ * Custom Exception
+ * @purpose To raise an exception when an item is not found in LinkedList
+ * @file DataNotFoundException.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 03/01/2020
+ */
 package com.bridgelabs.list
 
 class DataNotFoundException(message : String) : Throwable(message)
