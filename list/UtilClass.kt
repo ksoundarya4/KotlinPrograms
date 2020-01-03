3512cc78ea42c74191b06866e184514424b254fe
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-03 15:20:17 +0530

    [soundarya]Refactor. Added readNumberFile function to UtilClass.kt

--- list/UtilClass.kt
+++ list/UtilClass.kt
@@ -15,7 +15,7 @@ import java.io.PrintWriter
  * Function to read contents of file
  *
  * @param fileName To read the content from.
- * @return content of file using unorderedLinkedList.
+ * @return content of file in unordered LinkedList.
  */
 fun readFlie(fileName: String): UnorderedLinkedList<String> {
     val file = File(fileName)
@@ -29,6 +29,27 @@ fun readFlie(fileName: String): UnorderedLinkedList<String> {
         unorderedLinkedList.add(stringList[index])
     }
     return unorderedLinkedList
+}
+
+/**
+ *Function to return Ordered Linked list of numbers
+ *
+ * @param fileName Number file to read numbers.
+ * @return Ordered Linked List of numbers
+ */
+fun readNumberFile(fileName: String): OrderedLinkedList<Int> {
+    val file = File(fileName)
+    val bufferReader = file.bufferedReader()
+    val numbers = bufferReader.readLine()
+
+    val orderedLinkedList = OrderedLinkedList<Int>()
+    val numberList = numbers.split(" ")
+
+    for (index in 0.until(numberList.size)) {
+        orderedLinkedList.add(numberList[index].toInt())
+    }
+    return orderedLinkedList
+
 }
 
 /**
