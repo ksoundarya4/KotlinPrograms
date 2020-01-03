c917f468b0332368e3ced835f2a890505fab288f
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-03 17:42:50 +0530

    [soundarya]Refactor. Added file comment

--- list/OutOfSizeException.kt
+++ list/OutOfSizeException.kt
@@ -1,5 +1,11 @@
+/**
+ * Custom Exception
+ * @purpose To raise an exception when trying to access beyond linked list size.
+ * @file OutOfSizeException.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 02/01/2020
+ */
 package com.bridgelabs.list
 
-import java.lang.Exception
-
-class OutOfSizeException(override val message: String) : Exception(message)
+class OutOfSizeException(messge: String) : Throwable(messge)
