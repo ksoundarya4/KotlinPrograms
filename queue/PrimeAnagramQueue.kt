95e65bf0d9399b2e9beb3740d3289468330d7e69
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-04 17:48:58 +0530

    [soundarya]Add. Added PrimeAnagramQueue.kt

---
+++ queue/PrimeAnagramQueue.kt
@@ -0,0 +1,20 @@
+/**
+ * @description Add the Prime Numbers that are Anagram
+ * in the Range of 0 - 1000 in a Queue using the Linked
+ * List and Print the Anagrams from the Queue.
+ */
+package com.bridgelabs.queue
+
+import com.bridgelabs.list.OrderedLinkedList
+import com.bridgelabs.list.primeAnagram
+
+fun main() {
+    val primeAnagramList : OrderedLinkedList<Int> = primeAnagram()
+    val queue = Queue<Int>()
+
+    for(index in 0.until(primeAnagramList.size)) {
+        queue.enequeue(primeAnagramList.get(index))
+    }
+    for(index in 0.until(queue.size))
+        println(queue.dequeue())
+}
