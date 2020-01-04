e6cbc25bbd3f147c1445aa11f068dc90f086a5fe
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-04 17:51:55 +0530

    [soundarya]Refactor. Refactored file comments in PrimeAnagramQueue.kt

--- queue/PrimeAnagramQueue.kt
+++ queue/PrimeAnagramQueue.kt
@@ -2,6 +2,10 @@
  * @description Add the Prime Numbers that are Anagram
  * in the Range of 0 - 1000 in a Queue using the Linked
  * List and Print the Anagrams from the Queue.
+ * @file PrimeAnagramQueue.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 04/01/2020
  */
 package com.bridgelabs.queue
 
