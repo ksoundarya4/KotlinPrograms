51c1c3983e8a608ec125a71752cbb685305a9ddc
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-04 09:45:33 +0530

    [soundarya]Add. Added Person.kt

---
+++ queue/Person.kt
@@ -0,0 +1,13 @@
+/**
+ * Data Class
+ * @purpose To create A person class who have name and bank account
+ * @file Person.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 04/01/2020
+ */
+package com.bridgelabs.queue
+
+class Person (private  val name : String) {
+    val bankAccount = BankAccount()
+}
