87b10be5d6f9b8d584b41d60adf056a1d8a4c88c
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-07 13:06:50 +0530

    [soundarya]Refactor. Added file and function comments to BinaryTree.ky

--- BinaryTree.kt
+++ BinaryTree.kt
@@ -1,21 +1,34 @@
+/**
+ * Data Structure
+ * @purpose To implement binary search tree.
+ * @file BinaryTree.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 06/01/2020
+ */
 package com.bridgelabs
 
-
-class BinaryTree <T : Comparable<T>>{
+class BinaryTree<T : Comparable<T>> {
     class Node<T : Comparable<T>>(
         var data: T? = null,
         var leftNode: Node<T>? = null,
         var rightNode: Node<T>? = null
     )
 
-    fun insert(node : Node<T> , data : T){
-        if(data < node.data!!) {
-            if(node.leftNode != null){
+    /**
+     * Function to insert a node containing data into binary tree.
+     *
+     * @param node to be inserted
+     * @param data present in node
+     */
+    fun insert(node: Node<T>, data: T) {
+        if (data < node.data!!) {
+            if (node.leftNode != null) {
                 insert(node.leftNode!!, data)
-            }else{
+            } else {
                 node.leftNode = Node(data)
             }
-        }else if(data > node.data!!) {
+        } else if (data > node.data!!) {
             if (node.rightNode != null) {
                 insert(node.rightNode!!, data)
             } else {
@@ -23,21 +36,27 @@ class BinaryTree <T : Comparable<T>>{
             }
         }
     }
+
+    /**
+     * Function to traverse the binary tree
+     */
     fun traverseInOrder(node: Node<T>?) {
         if (node != null) {
             traverseInOrder(node.leftNode)
-            System.out.print(" " + node.data)
+            print(" " + node.data)
             traverseInOrder(node.rightNode)
         }
     }
+
 }
+
 
 fun main() {
     val tree = BinaryTree<Int>()
     val root = BinaryTree.Node(5)
     println(tree.traverseInOrder(root))
-    tree.insert(root,6)
-    tree.insert(root,2)
-    tree.insert(root,7)
+    tree.insert(root, 6)
+    tree.insert(root, 2)
+    tree.insert(root, 7)
     println(tree.traverseInOrder(root))
 }
