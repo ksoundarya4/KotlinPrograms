c4f18657f417ff22333b31ae9cd3102f43fbddbb
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-08 15:26:40 +0530

    [soundarya]Refactor. Added file and function comments to Name.kt

--- addressbook/Name.kt
+++ addressbook/Name.kt
@@ -1,22 +1,40 @@
+/**
+ * Object Oriented Programs
+ * @description Creating a data class Name
+ * that has firsName and lastName.
+ * @file Name.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 08/01/2020
+ */
 package com.bridgelabs.addressbook
 
 data class Name(
     val firstName: String? = null,
     val lastName: String? = null
 ) {
+    /**
+     * Function that overrides equals()
+     */
     override fun equals(other: Any?): Boolean {
-        if(other is Name){
-          if(other.firstName == this.firstName && other.lastName == this.lastName)
-              return true
+        if (other is Name) {
+            if (other.firstName == this.firstName && other.lastName == this.lastName)
+                return true
         }
         return false
     }
 
+    /**
+     * Function that overrides toString()
+     */
     override fun toString(): String {
         return "$firstName $lastName"
     }
 
+    /**
+     * Function that overrides hashCode()
+     */
     override fun hashCode(): Int {
-        return {firstName+lastName}.hashCode()
+        return { firstName + lastName }.hashCode()
     }
 }
