9cdb1cdd26017ddb81e0d3f484b5b890b1a702c3
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-08 11:16:07 +0530

    [soundarya]Add. Added PrintPrimeAnagram.kt

---
+++ test/PrintPrimeAnagram.kt
@@ -0,0 +1,16 @@
+/**
+ * Data Structures
+ * @purpose main function to print primeAnagrams using 2D array
+ * @file PrintPrimeAnagram.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 08/01/2020
+ */
+package com.bridgelabs.test
+
+import com.bridgelabs.two_d_array.PrimeAnagram
+
+fun main() {
+    val printPrimeAnagram = PrimeAnagram()
+    printPrimeAnagram.primeAnagramOf()
+}
