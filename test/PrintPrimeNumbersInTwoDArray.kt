5a227ffc19eae5b9121896b491d0490795ab9bbf
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-08 11:21:27 +0530

    [soundarya]Add. Added PrintPrimeNumbersInTwoDArray.kt

---
+++ test/PrintPrimeNumbersInTwoDArray.kt
@@ -0,0 +1,16 @@
+/**
+ * Data Structure
+ * @purpose main function to print prime numbers stored in 2D array
+ * @file PrintPrimeNumbersInTwoDArray.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 08/01/2020
+ */
+package com.bridgelabs.test
+
+import com.bridgelabs.two_d_array.PrimeNumbersInTwoDArray
+
+fun main() {
+    val printTwoDPrimeArray = PrimeNumbersInTwoDArray()
+    printTwoDPrimeArray.twoDPrimeArray()
+}
