821e2433cbf0e5e556484bcbace56ea230ef5d8c
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-08 16:05:45 +0530

    [soundarya]Refactor. Refactored Person.kt

--- addressbook/Person.kt
+++ addressbook/Person.kt
@@ -5,4 +5,25 @@ data class Person(
     val address: Address? = null,
     val phoneNumber: String? = null
 ) {
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Person) {
+            if (other.name!!.equals(this.name) &&
+                other.address!!.equals(this.address) &&
+                other.phoneNumber.equals(this.phoneNumber)
+            )
+                return true
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return "Name : $name \n Address : $address \n PhoneNumber : $phoneNumber"
+    }
+
+    override fun hashCode(): Int {
+        return {name.hashCode()+address.hashCode()+phoneNumber.hashCode()}.hashCode()
+    }
 }
+
+
