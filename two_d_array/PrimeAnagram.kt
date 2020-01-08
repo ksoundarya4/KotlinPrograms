4fb3ace65832fa167baa75df0a364c294575081a
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-08 11:11:43 +0530

    [soundarya]Refactor. Refactored PrimeAnagram.kt

--- two_d_array/PrimeAnagram.kt
+++ two_d_array/PrimeAnagram.kt
@@ -1,37 +1,39 @@
-@file:Suppress("UNUSED_CHANGED_VALUE")
+
 
 package com.bridgelabs.two_d_array
 
 import com.bridgelabs.util.primeAnagram
 import com.bridgelabs.util.primeNumbersOf
 
-fun main() {
-    val primeList = primeNumbersOf()
-    println(primeList)
-    val twoDPrimeAnagram = Array(2) { IntArray(primeList.size) }
-    val primeAnagramList = primeAnagram()
+class PrimeAnagram {
+    fun primeAnagramOf() {
+        val primeList = primeNumbersOf()
+        println(primeList)
+        val twoDPrimeAnagram = Array(2) { IntArray(primeList.size) }
+        val primeAnagramList = primeAnagram()
 
 
-    var count1 = 0
-    for (index in 0.until(primeAnagramList.size)) {
-        twoDPrimeAnagram[0][count1] = primeAnagramList.get(index)
-        count1++
-    }
-
-    var count2 = 0
-    for (index in 0.until(primeList.size)) {
-        if (!primeAnagramList.search(primeList.get(index))) {
-            twoDPrimeAnagram[1][count2] = primeList.get(index)
-            count2++
+        var count1 = 0
+        for (index in 0.until(primeAnagramList.size)) {
+            twoDPrimeAnagram[0][count1] = primeAnagramList.get(index)
+            count1++
         }
-    }
 
-    for (outerArray in 0.until(2)) {
-        for (innerArray in 0.until(primeAnagram().size)) {
-            if (twoDPrimeAnagram[outerArray][innerArray] != 0) {
-                print(" ${twoDPrimeAnagram[outerArray][innerArray]} ")
+        var count2 = 0
+        for (index in 0.until(primeList.size)) {
+            if (!primeAnagramList.search(primeList.get(index))) {
+                twoDPrimeAnagram[1][count2] = primeList.get(index)
+                count2++
             }
         }
-        println()
+
+        for (outerArray in 0.until(2)) {
+            for (innerArray in 0.until(primeAnagram().size)) {
+                if (twoDPrimeAnagram[outerArray][innerArray] != 0) {
+                    print(" ${twoDPrimeAnagram[outerArray][innerArray]} ")
+                }
+            }
+            println()
+        }
     }
 }
