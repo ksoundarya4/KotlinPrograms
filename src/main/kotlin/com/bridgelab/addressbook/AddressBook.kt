8df98fe451784b73c1c77fab1b108ad250553430
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-10 18:02:05 +0530

    [soundarya]Refactor. Added file and functional comment to AddressBook.kt

--- src/main/kotlin/com/bridgelab/addressbook/AddressBook.kt
+++ src/main/kotlin/com/bridgelab/addressbook/AddressBook.kt
@@ -1,6 +1,15 @@
+/**
+ * Object Oriented Programs
+ * @purpose To create a AddressBook that stores person information
+ * @file AddressBook.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 07/01/2020
+ */
 package com.bridgelab.addressbook
 
 import java.io.File
+import java.lang.IndexOutOfBoundsException
 
 class AddressBook() {
 
@@ -17,6 +26,9 @@ class AddressBook() {
         get
         private set
 
+    /**
+     * Function to add people to Address Book
+     */
     fun addPerson() {
 
         println("Enter Person Details")
@@ -63,49 +75,87 @@ class AddressBook() {
 
     }
 
-    fun getFullNameOfPerson(index: Int): String {
-        val person: Person = persons.get(index)
-        return person.name.toString()
+    /**
+     * Function to get full name of person
+     *
+     * @param index of person ArrayList
+     */
+    fun getFullNameOfPerson(index: Int): String? {
+        try {
+            val person: Person = persons.get(index)
+            return person.name.toString()
+        } catch (exception: IndexOutOfBoundsException) {
+            println("First index  = 0 and last index = ${numberOfPerson - 1}")
+            exception.stackTrace
+        }
+        return null
     }
 
+    /**
+     * Function to update person info based on ArrayIndex
+     */
     fun updatePerson(index: Int) {
+        try {
+            val id = readLine()!!.toInt()
+            val firstName = readLine()!!.toString()
+            val lastName = readLine()!!.toString()
+            val houseNumber = readLine()!!.toString()
+            val street = readLine()!!.toString()
+            val locality = readLine()!!.toString()
+            val city = readLine()!!.toString()
+            val state = readLine()!!.toString()
+            val zipCode = readLine()!!.toString()
+            val phoneNumber = readLine()!!.toString()
 
-        val id = readLine()!!.toInt()
-        val firstName = readLine()!!.toString()
-        val lastName = readLine()!!.toString()
-        val houseNumber = readLine()!!.toString()
-        val street = readLine()!!.toString()
-        val locality = readLine()!!.toString()
-        val city = readLine()!!.toString()
-        val state = readLine()!!.toString()
-        val zipCode = readLine()!!.toString()
-        val phoneNumber = readLine()!!.toString()
+            persons.set(index, Person(
+                    id = id,
+                    name = Name(firstName, lastName),
+                    address = Address(houseNumber, street, locality, city, state, zipCode
+                    ),
+                    phoneNumber = phoneNumber))
 
-        persons.set(index, Person(
-                id = id,
-                name = Name(firstName, lastName),
-                address = Address(houseNumber, street, locality, city, state, zipCode
-                ),
-                phoneNumber = phoneNumber))
-
+        } catch (exception: IndexOutOfBoundsException) {
+            println("First index  = 0 and last index = ${numberOfPerson - 1}")
+            exception.stackTrace
+        }
     }
 
+    /**
+     * Function to remove a person from
+     * Address Book based on Array Index
+     */
     fun removePerson(index: Int) {
-        persons.removeAt(index)
+        try {
+            persons.removeAt(index)
+        } catch (exception: IndexOutOfBoundsException) {
+            println("First index  = 0 and last index = ${numberOfPerson - 1}")
+            exception.stackTrace
+        }
     }
 
+    /**
+     *
+     * function to sort AddressBook by person name
+     */
     fun sortByName() {
         persons.sortWith(compareBy { it.name.firstName })
     }
 
+    /**
+     *
+     * function to sort AddressBook by
+     * person address zip code
+     */
     fun sortByZip() {
         persons.sortWith(compareBy { it.address.zip })
     }
 
+    /**
+     * Function to print all persons details
+     * present in Address Book
+     */
     fun printAll() {
         for (index in 0.until(persons.size))
             println(persons.get(index))
     }
-
-
 }
