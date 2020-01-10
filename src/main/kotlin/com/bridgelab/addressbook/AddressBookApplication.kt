04bbaf45c31ad30c0f7ec11f787ac743432e4222
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-10 18:31:05 +0530

    [soundarya]Add. Added AddressBookApplication.kt

---
+++ src/main/kotlin/com/bridgelab/addressbook/AddressBookApplication.kt
@@ -0,0 +1,14 @@
+/**
+ *Object Oriented Programs
+ * @purpose To create Address Book Application
+ * @file AddressBookApplication.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 10/02/2020
+ */
+package com.bridgelab.addressbook
+
+fun main() {
+    val addressBookApplication = AddressBookController()
+    addressBookApplication.addressBookController()
+}
