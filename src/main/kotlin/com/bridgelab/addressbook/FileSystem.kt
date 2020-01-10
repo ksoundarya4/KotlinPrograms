4bb3c17f5d21f03dfa9a1fa67bb6711124053bf7
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-10 17:45:45 +0530

    [soundarya]Refactor. refactored FileSystem.kt

--- src/main/kotlin/com/bridgelab/addressbook/FileSystem.kt
+++ src/main/kotlin/com/bridgelab/addressbook/FileSystem.kt
@@ -1,5 +1,24 @@
 package com.bridgelab.addressbook
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
+import java.lang.Exception
+
 class FileSystem {
 
+    val objectMapper = jacksonObjectMapper()
+
+    fun saveFile(addressBook: AddressBook, file: File) {
+        try {
+
+            objectMapper.writeValue(file, addressBook)
+        } catch (exception: Exception) {
+            exception.stackTrace
+        }
+    }
+
+    fun readFile(file: File): AddressBook {
+        return objectMapper.readValue(file)
+    }
 }
