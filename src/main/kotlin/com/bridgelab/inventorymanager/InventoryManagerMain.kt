703504f77f761aeab9a1d089da71d4c4ef8557c9
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 13:30:36 +0530

    [soundarya]Refactor. Refactored InventoryManagerMain.kt

--- src/main/kotlin/com/bridgelab/inventorymanager/InventoryManagerMain.kt
+++ src/main/kotlin/com/bridgelab/inventorymanager/InventoryManagerMain.kt
@@ -1,4 +1,15 @@
 package com.bridgelab.inventorymanager
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
 fun main() {
-     val inventoryManager = InventoryManager()
+    val inventoryManager = InventoryManager()
+
+    val objectMapping = jacksonObjectMapper()
+
+    val jsonRicePrice = inventoryManager.calculateRicePrice()
+    val jsonWheatPrice = inventoryManager.calculateWheatPrice()
+    val jsonPulsePrice = inventoryManager.calculatePulsesPrice()
+    println(" { $jsonRicePrice $jsonWheatPrice $jsonPulsePrice }")
+
 }
