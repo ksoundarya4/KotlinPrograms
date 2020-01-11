345a28a9da904b28c05f4c86194fd5b37ee42ac6
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 18:37:16 +0530

    [soundarya]Refactor. added overridden functions to Appointment.kt

--- src/main/kotlin/com/bridgelab/clinicManagement/Appointment.kt
+++ src/main/kotlin/com/bridgelab/clinicManagement/Appointment.kt
@@ -5,5 +5,19 @@ import java.util.*
 class Appointment(val doctorId: Int,
                   val date: Date) {
 
+    override fun toString(): String {
+        return "Appointment : $doctorId , $date"
+    }
 
+    override fun equals(other: Any?): Boolean {
+        if (other is Appointment) {
+            if (other.doctorId == this.doctorId &&
+                    other.date == this.date) return true
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return {doctorId.toString()+date.toString()}.hashCode()
+    }
 }
