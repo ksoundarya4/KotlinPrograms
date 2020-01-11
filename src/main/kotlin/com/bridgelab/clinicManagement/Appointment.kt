efdf0a113874d22fa1776d10b211c55b576391e0
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 18:45:03 +0530

    [soundarya]Refactor. Added file and function comment to Appointment.kt

--- src/main/kotlin/com/bridgelab/clinicManagement/Appointment.kt
+++ src/main/kotlin/com/bridgelab/clinicManagement/Appointment.kt
@@ -1,14 +1,28 @@
+/**
+ * Object Oriented Program
+ * @purpose To create a Appointment class
+ * having doctorId and date of appointment
+ * @file Appointment.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 11/02/2020
+ */
 package com.bridgelab.clinicManagement
 
 import java.util.*
 
 class Appointment(val doctorId: Int,
                   val date: Date) {
-
+    /**
+     * Function to override toString function
+     */
     override fun toString(): String {
         return "Appointment : $doctorId , $date"
     }
 
+    /**
+     * Function to override equals function
+     */
     override fun equals(other: Any?): Boolean {
         if (other is Appointment) {
             if (other.doctorId == this.doctorId &&
@@ -17,6 +31,9 @@ class Appointment(val doctorId: Int,
         return false
     }
 
+    /**
+     * Function to override hashCode function
+     */
     override fun hashCode(): Int {
         return {doctorId.toString()+date.toString()}.hashCode()
     }
