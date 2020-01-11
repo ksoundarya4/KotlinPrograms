9d80b63cdd03f7e108575956dba563f430a2ead8
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 16:33:44 +0530

    [soundarya]Add. Added Doctor.kt

---
+++ src/main/kotlin/com/bridgelab/clinicManagement/Doctor.kt
@@ -0,0 +1,7 @@
+package com.bridgelab.clinicManagement
+
+class Doctor(val name: String,
+             val doctorID: Int,
+             val specialization: String,
+             val Availability: String) {
+}
