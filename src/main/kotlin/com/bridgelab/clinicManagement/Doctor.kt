bf483d70d5d9384b4dc6c5ce19817b2ed0553a40
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 16:39:50 +0530

    [soundarya]Refactor. Refactored Doctor.kt

--- src/main/kotlin/com/bridgelab/clinicManagement/Doctor.kt
+++ src/main/kotlin/com/bridgelab/clinicManagement/Doctor.kt
@@ -3,5 +3,29 @@ package com.bridgelab.clinicManagement
 class Doctor(val name: String,
              val doctorID: Int,
              val specialization: String,
-             val Availability: String) {
+             val availability: String) {
+
+    var patientList: List<Patient>? = null
+
+    override fun toString(): String {
+        return "Doctor : $doctorID,$name,$specialization,$availability"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Doctor) {
+            if (
+                    other.name == this.name &&
+                    other.doctorID == this.doctorID &&
+                    other.specialization == this.specialization &&
+                    other.availability == this.availability
+            )
+                return true
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return {name+doctorID.toString()+specialization+availability}.hashCode()
+    }
+
 }
