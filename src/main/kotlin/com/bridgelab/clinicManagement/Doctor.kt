51c0b1db769cb13349c606222510edddc6dd7930
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 17:03:34 +0530

    [soundarya]Refactor. Added file and function comments to Doctor.kt

--- src/main/kotlin/com/bridgelab/clinicManagement/Doctor.kt
+++ src/main/kotlin/com/bridgelab/clinicManagement/Doctor.kt
@@ -1,3 +1,12 @@
+/**
+ * Object Oriented Program
+ * @purpose To crete a Doctor class having
+ * name, doctor id, specialization, availability.
+ * @file Doctor.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 11/01/2020
+ */
 package com.bridgelab.clinicManagement
 
 class Doctor(val name: String,
@@ -7,10 +16,17 @@ class Doctor(val name: String,
 
     var patientList: List<Patient>? = null
 
+    /**
+     * Function to override toString function
+     * To print doctor details
+     */
     override fun toString(): String {
         return "Doctor : $doctorID,$name,$specialization,$availability"
     }
 
+    /**
+     * Function to override equals function
+     */
     override fun equals(other: Any?): Boolean {
         if (other is Doctor) {
             if (
@@ -24,6 +40,9 @@ class Doctor(val name: String,
         return false
     }
 
+    /**
+     * Function to override hahCode function
+     */
     override fun hashCode(): Int {
         return {name+doctorID.toString()+specialization+availability}.hashCode()
     }
