0be0e805f293d90c85a54ca9c92f6dff747bf5c7
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-11 16:31:47 +0530

    [soundarya]Refactor. Added file and function comments to Patient.kt

--- src/main/kotlin/com/bridgelab/clinicManagement/Patient.kt
+++ src/main/kotlin/com/bridgelab/clinicManagement/Patient.kt
@@ -1,3 +1,12 @@
+/**
+ * Object Oriented Program
+ * @purpose To create a patient class having
+ * name, patient id, mobile number and age.
+ * @file Patient.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 11/02/2020
+ */
 package com.bridgelab.clinicManagement
 
 data class Patient(val name: String,
@@ -5,10 +14,17 @@ data class Patient(val name: String,
                    val mobileNumber: String,
                    val age: Int) {
 
+    /**
+     * Function to override toString function
+     * To print patient details
+     */
     override fun toString(): String {
         return "patient : $patientID,$name,$mobileNumber,$age"
     }
 
+    /**
+     *Function to override equals function
+     */
     override fun equals(other: Any?): Boolean {
         if (other is Patient) {
             if (
@@ -21,6 +37,9 @@ data class Patient(val name: String,
         return false
     }
 
+    /**
+     * Function to override hashCode function
+     */
     override fun hashCode(): Int {
         return {patientID.toString()+name+mobileNumber+age.toString()}.hashCode()
     }
