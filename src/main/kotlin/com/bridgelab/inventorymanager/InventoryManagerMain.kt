8c46f51e792dee4a4b559fcfa4d1ee6ace5c8078
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-12 16:15:46 +0530

    [soundarya]Refactor. Added file comment to InventoryManagerMain.kt

--- src/main/kotlin/com/bridgelab/inventorymanager/InventoryManagerMain.kt
+++ src/main/kotlin/com/bridgelab/inventorymanager/InventoryManagerMain.kt
@@ -1,5 +1,12 @@
+/**
+ * Object Oriented Program
+ * @purpose main function to execute Inventory Manager
+ * @file InventoryManagerMain.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 11/01/2020
+ */
 package com.bridgelab.inventorymanager
-
 
 fun main() {
     val inventoryManager = InventoryManager()
