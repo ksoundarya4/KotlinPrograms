38cf695e757c810144ccf905a93a50a9420ce6e1
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-12 16:04:06 +0530

    [soundarya]Refactor. Added file comments to InventoryFactory.kt

--- src/main/kotlin/com/bridgelab/inventorymanager/InventoryFactory.kt
+++ src/main/kotlin/com/bridgelab/inventorymanager/InventoryFactory.kt
@@ -1,3 +1,11 @@
+/**
+ * Object Oriented Program
+ * @description Inventory factor that contains list of items.
+ * @file InventoryFactory.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 11/01/2020
+ */
 package com.bridgelab.inventorymanager
 
 data class Item(val Name: String?,
