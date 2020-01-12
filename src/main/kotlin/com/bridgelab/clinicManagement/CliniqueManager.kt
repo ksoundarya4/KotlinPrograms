c3e51768d38002fc2ba7f70219dd36aa632cd17f
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-12 15:33:18 +0530

    [soundarya]Refactor. Added file comments to CliniqueManager.kt

--- src/main/kotlin/com/bridgelab/clinicManagement/CliniqueManager.kt
+++ src/main/kotlin/com/bridgelab/clinicManagement/CliniqueManager.kt
@@ -1,3 +1,20 @@
+/**
+ * Object Oriented Program
+ * @description Clinique Management Programme.
+ * This programme is used to manage a list of Doctors associated with the Clinique.
+ * This also manages the list of patients who use the clinique. It manages Doctors
+ * by Name, Id, Specialization and Availability (AM,  PM or both). It manages Patients
+ * by Name, ID, Mobile Number and Age. The Program allows users to search Doctor by name,
+ * id, Specialization or Availability. Also the programs allows users to search patient by
+ * name, mobile number or id. The programs allows patients to take appointment with the
+ * doctor. A doctor at  any availability time can see only 5 patients. If exceeded the
+ * user can take appointment for patient at different date or availability time.
+ * Print the Doctor Patient Report.
+ * @file CliniqueManager.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 11/01/2020
+ */
 package com.bridgelab.clinicManagement
 
 import java.time.LocalDate
