36aa6c687e2f31997aac31ac5a7690712be1a5fd
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 15:49:52 +0530

    [soundarya]Add. Added util Package

---
+++ src/main/kotlin/com/bridgelab/util/DataNotFoundException.kt
@@ -0,0 +1,12 @@
+/**
+ * Custom Exception
+ * @purpose To raise an exception when an item is not found in LinkedList
+ * @file DataNotFoundException.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 03/01/2020
+ */
+package com.bridgelabs.util
+
+class DataNotFoundException(message : String) : Throwable(message)
+
