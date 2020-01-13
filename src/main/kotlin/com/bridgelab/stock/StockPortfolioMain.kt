335272eefcd56a42ea19cdd747cc5611cb28bc3e
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 18:30:45 +0530

    [soundarya]Refactor. Added file comment to StockPortfolioMain.kt

--- src/main/kotlin/com/bridgelab/stock/StockPortfolioMain.kt
+++ src/main/kotlin/com/bridgelab/stock/StockPortfolioMain.kt
@@ -1,7 +1,15 @@
+/**
+ * Object Oriented Program
+ * @purpose Main function to execute stock portfolio
+ * @file StockPortfolioMain.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/01/2020
+ */
 package com.bridgelab.stock
 
 fun main() {
-     var stockPortfolio = StockPortfolio()
+    var stockPortfolio = StockPortfolio()
     val fileSystem = FileSystem()
     stockPortfolio = fileSystem.readFile(stockPortfolio.file)
     stockPortfolio.valueOfEachStock()
