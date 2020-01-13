2cc1c57620323103c7d6f1a3e564d8e70270514a
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 16:09:12 +0530

    [soundarya]Refactor. Refactored DeckOfCardsMain.kt

--- src/main/kotlin/com/bridgelab/deckofcards/DeckOfCardsMain.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/DeckOfCardsMain.kt
@@ -1,9 +1,21 @@
+/**
+ * Object Oriented Program
+ * @purpose Main function to initialize , shuffle
+ * and distribute the deck of cards.
+ * Print the cards of players
+ * @file DeckOfCardsMain.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/01/2020
+ */
 package com.bridgelab.deckofcards
+
 fun main(){
     val deckOfCards =DeckOfCards()
     deckOfCards.initialize()
     deckOfCards.shuffle()
  val player =  deckOfCards.distribute()
+
      for(index in 0.until(player.size)){
          println("Player ${index+1}")
          println(player[index])
