cb81c94352312fca1b2fa5ba4895bff621a6f1bb
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 18:18:29 +0530

    [soundarya]Refactor. Added file and function comment to FileSystem.kt

--- src/main/kotlin/com/bridgelab/stock/FileSystem.kt
+++ src/main/kotlin/com/bridgelab/stock/FileSystem.kt
@@ -1,5 +1,10 @@
 /**
  * Object Oriented Program
+ * @description To read and write json file.
+ * @file FileSystem.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/01/2020
  */
 package com.bridgelab.stock
 
@@ -10,6 +15,12 @@ import java.io.File
 class FileSystem {
     val objectMapper = jacksonObjectMapper()
 
+    /**
+     * Function to write stock portfolio to stock.json file
+     *
+     * @param stockPortfolio class
+     * @param file path of .json file
+     */
     fun saveFile(stockPortfolio: StockPortfolio , file : File){
         try{
             objectMapper.writeValue(file,stockPortfolio)
@@ -18,6 +29,12 @@ class FileSystem {
         }
     }
 
+    /**
+     * Function to read .json file
+     *
+     * @param file to be read
+     * @return StockPortfolio
+     */
     fun readFile(file : File) : StockPortfolio{
         return objectMapper.readValue(file)
     }
