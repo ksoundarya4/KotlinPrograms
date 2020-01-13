443c7a002937a06d585cf5b861a7a3bb7bafd036
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 15:32:38 +0530

    [soundarya]Refactor. Refactored DeckOfCards.kt

--- src/main/kotlin/com/bridgelab/deckofcards/DeckOfCards.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/DeckOfCards.kt
@@ -3,36 +3,37 @@ package com.bridgelab.deckofcards
 class DeckOfCards {
 
     private val suite: Array<String> = arrayOf("Clubs", "Diamonds", "Hearts", "Spades")
-    private val rank: Array<String> = arrayOf("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King")
+    private val rank: Array<String> = arrayOf("Ace", "King", "Queen", "jack", "2", "3", "4", "5", "6", "7", "8", "9", "10")
     private val numberOfCards = suite.size * rank.size
 
-   var listOfCards = ArrayList<Card>()
+    var list = ArrayList<Card>()
 
     fun initialize() {
         for (indexI in 0.until(suite.size)) {
             for (indexJ in 0.until(rank.size)) {
-                listOfCards.add(Card(suite[indexI], rank[indexJ]))
+                list.add(Card(suite[indexI], rank[indexJ]))
             }
         }
     }
 
-    fun shuffle(){
-        for(index in 0.until(numberOfCards)){
-            val randomNumber = index + (Math.random() * (numberOfCards-index)).toInt()
-            val temp = listOfCards[randomNumber]
-            listOfCards[randomNumber] = listOfCards[index]
-            listOfCards[index] = temp
+    fun shuffle() {
+        for (index in 0.until(numberOfCards)) {
+            val randomNumber = index + (Math.random() * (numberOfCards - index)).toInt()
+            val temp = list[randomNumber]
+            list[randomNumber] = list[index]
+            list[index] = temp
         }
     }
 
-    fun distribute() {
-        for(indexI in 0.until(4)){
-            println("Person ${indexI+1} : ")
-            println()
-            for(indexJ in 0.until(9)){
-                println(listOfCards[indexI+indexJ*4])
+    fun distribute(): ArrayList<Player> {
+        val players = ArrayList<Player>()
+        for (indexI in 0.until(4)) {
+            val player = Player()
+            for (indexJ in 0.until(9)) {
+                player.listOfPlayerCards.add(list[indexI + indexJ * 4])
             }
-            println()
+            players.add(player)
         }
+        return players
     }
 }
