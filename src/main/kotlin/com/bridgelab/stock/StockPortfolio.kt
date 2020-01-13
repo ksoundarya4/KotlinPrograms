9907983b69a090c6abb8201125c46acf8fffd2d5
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 18:28:49 +0530

    [soundarya]Refactor. Added file and function comments to StockPortfolio.kt

--- src/main/kotlin/com/bridgelab/stock/StockPortfolio.kt
+++ src/main/kotlin/com/bridgelab/stock/StockPortfolio.kt
@@ -1,3 +1,13 @@
+/**
+ * Object Oriented Program
+ * @description Write a program to read in Stock Names,
+ * Number of Share, Share Price. Print a Stock Report with
+ * total value of each Stock and the total value of Stock.
+ * @file StockPortfolio.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/01/2020
+ */
 package com.bridgelab.stock
 
 import java.io.File
@@ -8,6 +18,10 @@ class StockPortfolio {
     var file = File("/home/user/IdeaProjects/KotlinPrograms/src/main/kotlin/com/bridgelab/stock/stock.json")
     var totalPrice: Int = 0
 
+    /**
+     *Function to calculate price of each stock
+     * and print them
+     */
     fun valueOfEachStock() {
         for (stock in stockList) {
             val priceOfStock = stock.numberOfShares.times(stock.sharePrice)
@@ -16,6 +30,10 @@ class StockPortfolio {
         }
     }
 
+    /**
+     * Function to override toString function
+     * To print list of stocks.
+     */
     override fun toString(): String {
         return stockList.toString()
     }
