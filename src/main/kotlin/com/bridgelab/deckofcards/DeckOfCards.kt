bd03c739b1d40c162da1a37eb502c8b4fd221e7e
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 12:44:40 +0530

    [soundarya]Refactor. Added initialize,shuffle and distribute functions to DeckOfCards.kt

--- src/main/kotlin/com/bridgelab/deckofcards/DeckOfCards.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/DeckOfCards.kt
@@ -1,4 +1,38 @@
 package com.bridgelab.deckofcards
 
 class DeckOfCards {
+
+    private val suite: Array<String> = arrayOf("Clubs", "Diamonds", "Hearts", "Spades")
+    private val rank: Array<String> = arrayOf("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King")
+    private val numberOfCards = suite.size * rank.size
+
+   var listOfCards = ArrayList<Card>()
+
+    fun initialize() {
+        for (indexI in 0.until(suite.size)) {
+            for (indexJ in 0.until(rank.size)) {
+                listOfCards.add(Card(suite[indexI], rank[indexJ]))
+            }
+        }
+    }
+
+    fun shuffle(){
+        for(index in 0.until(numberOfCards)){
+            val randomNumber = index + (Math.random() * (numberOfCards-index)).toInt()
+            val temp = listOfCards[randomNumber]
+            listOfCards[randomNumber] = listOfCards[index]
+            listOfCards[index] = temp
+        }
+    }
+
+    fun distribute() {
+        for(indexI in 0.until(4)){
+            println("Person ${indexI+1} : ")
+            println()
+            for(indexJ in 0.until(9)){
+                println(listOfCards[indexI+indexJ*4])
+            }
+            println()
+        }
+    }
 }
