b81ac91ddca0a5c55669bdf9b8f0503e74c63ff4
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 16:27:55 +0530

    [soundarya]Refactor. Added file anf functional comment

--- src/main/kotlin/com/bridgelab/deckofcards/Card.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/Card.kt
@@ -1,11 +1,26 @@
+/**
+ * Object Oriented Programs
+ * @description Card class which has suit and a rank
+ * @file Card.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/01/2020
+ */
 package com.bridgelab.deckofcards
 
 data class Card(val suit: String, val rank: String) {
 
+    /**
+     * Function to override toString function
+     * To print rank and suit of card
+     */
     override fun toString(): String {
         return "${this.rank} of ${this.suit}"
     }
 
+    /**
+     * Function to override equals function
+     */
     override fun equals(other: Any?): Boolean {
         if (other is Card) {
             if (other.suit == this.suit &&
@@ -14,6 +29,9 @@ data class Card(val suit: String, val rank: String) {
         return false
     }
 
+    /**
+     * Function to override hashCode function
+     */
     override fun hashCode(): Int {
         return {this.rank+this.suit}.hashCode()
     }
