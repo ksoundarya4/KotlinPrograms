740b71c5d3039bf43e00a9d6bc897e2e27596c13
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 16:36:59 +0530

    [soundarya]Refactor. Added file and function comments

--- src/main/kotlin/com/bridgelab/deckofcards/Player.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/Player.kt
@@ -1,13 +1,27 @@
+/**
+ * Object Oriented Program
+ * @description A Player class having list of cards.
+ * @file Player.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/01/2020
+ */
 package com.bridgelab.deckofcards
 
 class Player {
     var listOfPlayerCards = ArrayList<Card>()
-
+    /**
+     * Function to override toString function
+     * To print list Of  Player Cards
+     */
     override fun toString(): String {
         return listOfPlayerCards.toString()
     }
 
-    fun sortBysuit() {
+    /**
+     * Function to sort list of player cards based on suit.
+     */
+    fun sortBySuit() {
       listOfPlayerCards.sortBy { it.suit }
         }
 
