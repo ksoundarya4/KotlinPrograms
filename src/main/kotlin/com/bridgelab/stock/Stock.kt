d69659ce220eb82518c1b446faad70bcf89a3216
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-13 17:30:07 +0530

    [soundarya]Refactor. Refactored Stock.kt

--- src/main/kotlin/com/bridgelab/stock/Stock.kt
+++ src/main/kotlin/com/bridgelab/stock/Stock.kt
@@ -1,4 +1,22 @@
 package com.bridgelab.stock
 
-class Stock {
+data class Stock(val stockName: String,
+                 val numberOfShares: Int,
+                 val sharePrice: Int) {
+    override fun toString(): String {
+        return "$stockName , $numberOfShares , $sharePrice"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Stock) {
+            if (other.stockName == this.stockName &&
+                    other.numberOfShares == this.numberOfShares &&
+                    other.sharePrice == this.sharePrice) return true
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return (stockName+numberOfShares.toString()+sharePrice.toString()).hashCode()
+    }
 }
