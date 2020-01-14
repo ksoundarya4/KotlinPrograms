f4dd4556f982ca2ae5ba3eb18f2d2d376346a874
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-14 15:10:51 +0530

    [soundarya]Refactored. Added Intendation to CardGame.kt

--- src/main/kotlin/com/bridgelab/deckofcards/CardGame.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/CardGame.kt
@@ -10,16 +10,14 @@
  */
 package com.bridgelab.deckofcards
 
-fun main(){
-    val deckOfCards =DeckOfCards()
+fun main() {
+
+    val deckOfCards = DeckOfCards()
     deckOfCards.initialize()
     deckOfCards.shuffle()
- val player =  deckOfCards.distribute()
-
-     for(index in 0.until(player.size)){
-         println("Player ${index+1}")
-         println(player[index])
-     }
-
-
+    val player = deckOfCards.distribute()
+    for (index in 0.until(player.size)) {
+        println("Player ${index + 1}")
+        println(player[index])
+    }
 }
