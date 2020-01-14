35d816fd8b9ae60cbc29dedeffe5e0a9b6a197a4
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-14 15:20:48 +0530

    [soundarya]Refactor. Added file comment to Suit.kt

--- src/main/kotlin/com/bridgelab/deckofcards/Suit.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/Suit.kt
@@ -1,3 +1,11 @@
+/**
+ * Object Oriented Program
+ * @purpose enum class to get suit of card.
+ * @file  Suit.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 14/01/2020
+ */
 package com.bridgelab.deckofcards
 
 enum class Suit {
