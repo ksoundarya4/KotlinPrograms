557165eccc02d20543d79e2c4190922b79c40a05
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-14 15:20:11 +0530

    [soundarya]Refactor. Added file comments to Rank.kt

--- src/main/kotlin/com/bridgelab/deckofcards/Rank.kt
+++ src/main/kotlin/com/bridgelab/deckofcards/Rank.kt
@@ -1,3 +1,11 @@
+/**
+ * Object Oriented Program
+ * @purpose enum class to get rank of card
+ * @file  Rank.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 14/01/2020
+ */
 package com.bridgelab.deckofcards
 
 enum class Rank {
