606585280600cd811db973a4c5ea76b4392982bf
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-16 15:46:44 +0530

    [soundarya]Add. Added FactoryMethodTest.kt

---
+++ src/test/kotlin/designpatterns/factory/FactoryMethodTest.kt
@@ -0,0 +1,18 @@
+package designpatterns.factory
+
+import org.junit.Test
+import org.assertj.core.api.Assertions.assertThat
+
+
+class FactoryMethodTest{
+
+    @Test
+    fun factoryMethod(){
+
+        val indianCurrency = CurrencyFactory.currencyForCountry(India()).code
+        val usaCurrency = CurrencyFactory.currencyForCountry(USA()).code
+
+        assertThat(indianCurrency).isEqualTo("INR")
+        assertThat(usaCurrency).isEqualTo("USD")
+    }
+}
