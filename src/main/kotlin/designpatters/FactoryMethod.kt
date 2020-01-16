8a4d12cd77470a2631f4720d6ef58a9d0b1984cd
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-16 15:24:07 +0530

    [soundarya]Refactor. Refactored FactoryMethod.kt

--- src/main/kotlin/designpatters/FactoryMethod.kt
+++ src/main/kotlin/designpatters/FactoryMethod.kt
@@ -1,4 +1,28 @@
 package designpatters
 
-class FactoryMethod {
+interface Country
+class USA : Country
+class Greece : Country
+class India : Country
+
+class Currency(val code : String){
+    override fun toString(): String {
+        return code
+    }
+}
+
+
+object CurrencyFactory {
+
+    fun currencyForCountry(country : Country) : Currency =
+        when(country){
+            is USA -> Currency("USD")
+            is Greece -> Currency("EUR")
+            is India -> Currency("INR")
+            else ->  Currency("None")
+        }
+}
+
+fun main() {
+    println(CurrencyFactory.currencyForCountry(India()))
 }
