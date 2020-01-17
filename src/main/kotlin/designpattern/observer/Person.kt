a4e14cb46eb1f27d19913b50d9273a4d6f2a536f
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-17 11:54:49 +0530

    [soundarya]Add. Added Person.kt

---
+++ src/main/kotlin/designpattern/observer/Person.kt
@@ -0,0 +1,22 @@
+/**
+ * Design Pattern
+ * @purpose To implement observer design pattern
+ * @file Person.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 17/01/2020
+ */
+package designpattern.observer
+
+/**
+ * Person class having name implements Observer
+ */
+class Person(val name : String) : Observer {
+
+    /**
+     * Function that override update function
+     */
+    override fun update(availability: String) {
+        println("Hello $name, is $availability in store")
+    }
+}
