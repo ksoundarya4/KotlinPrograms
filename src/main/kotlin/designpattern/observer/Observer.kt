d46c894eb92e97aaf3dd4226ef76baaf14374963
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-17 11:47:49 +0530

    [soundarya]Add. Added Observer.kt interface

---
+++ src/main/kotlin/designpattern/observer/Observer.kt
@@ -0,0 +1,14 @@
+/**
+ * Design Pattern
+ * @purpose To implement observer design pattern
+ * @file Observer.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 17/01/2020
+ */
+package designpattern.observer
+
+interface Observer {
+
+    fun update(availability : String)
+}
