06cb95ac394735a973f4c60ffc24a3a6da775ff8
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-18 12:15:39 +0530

    [soundarya]Add. Added Authorized.kt

---
+++ src/main/kotlin/designpattern/state/Authorized.kt
@@ -0,0 +1,11 @@
+/**
+ * Design Pattern
+ * @purpose to implement state design pattern.
+ * @file Authorized.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 18/01/2020
+ */
+package designpattern.state
+
+class Authorized(val userName: String) : AuthorizationState
