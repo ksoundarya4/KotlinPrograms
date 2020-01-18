c2d4b69fb1ac7a4ae7f475b72b62560c5d178820
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-18 16:40:14 +0530

    [soundarya]Add. Added OrderCommand.kt

---
+++ src/main/kotlin/designpattern/command/OrderCommand.kt
@@ -0,0 +1,13 @@
+/**
+ * Design Pattern
+ * @purpose To implement command design pattern.
+ * @file ORderCommand.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 18/01
+ */
+package designpattern.command
+
+interface OrderCommand {
+    fun execute()
+}
