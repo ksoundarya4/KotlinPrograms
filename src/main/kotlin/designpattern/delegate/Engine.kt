ce7bef12c60f102a97130da28050486abb781864
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-18 17:55:48 +0530

    [soundarya]Add. Add Engine.kt interface

---
+++ src/main/kotlin/designpattern/delegate/Engine.kt
@@ -0,0 +1,16 @@
+/**
+ * Design Pattern
+ * @purpose To Implement delegate design pattern.
+ * @file Engine.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 18/01/2020
+ */
+package designpattern.delegate
+
+/**
+ * Engine Interface
+ */
+interface Engine{
+    fun start()
+}
