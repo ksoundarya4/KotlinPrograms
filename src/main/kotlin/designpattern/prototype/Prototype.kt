1e9fd9a0ef87f1f538e77370d5050202799d3c56
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-18 09:58:19 +0530

    [soundarya]Add. Added Prototype.kt interface

---
+++ src/main/kotlin/designpattern/prototype/Prototype.kt
@@ -0,0 +1,16 @@
+/**
+ * Design Pattern
+ * @purpose to implement prototype design pattern.
+ * @file Prototype.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 18/01/2020
+ */
+package designpattern.prototype
+
+/**
+ * Prototype Interface having clone function.
+ */
+interface Prototype {
+    fun clone()
+}
