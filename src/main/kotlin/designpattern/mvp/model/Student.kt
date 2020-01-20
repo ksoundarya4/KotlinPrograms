d8dd79b69204cb47418a51f37fd6c393c4f6d26f
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-20 18:46:09 +0530

    [soundarya]Refactor. Refactored Student.kt

--- src/main/kotlin/designpattern/mvp/model/Student.kt
+++ src/main/kotlin/designpattern/mvp/model/Student.kt
@@ -1,3 +1,11 @@
+/**
+ * Design Pattern
+ * @purpose To implement MVP design pattern.
+ * @file ksoundarya4
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 20/01/2020
+ */
 package designpattern.mvp.model
 
 data class Student(val name : String, val rollNumber : String)
