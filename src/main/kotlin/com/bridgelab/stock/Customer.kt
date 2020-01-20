868cdde67cc42be796ab1edcad0d449830e5fec4
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-20 22:53:22 +0530

    [soundarya]Refactor. Added hashCode function to Customer.kt

--- src/main/kotlin/com/bridgelab/stock/Customer.kt
+++ src/main/kotlin/com/bridgelab/stock/Customer.kt
@@ -1,4 +1,15 @@
+/**
+ * Object Oriented Programs
+ * @description Customer data class having customer name
+ * and stock account
+ * @file Customer.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 19/01/2020
+ */
 package com.bridgelab.stock
+
+import java.util.Queue
 
 data class Customer(val name: String, val stockAccount: StockAccount) {
 
@@ -13,8 +24,18 @@ data class Customer(val name: String, val stockAccount: StockAccount) {
         return false
     }
 
+    /**
+     * Function to override toString function
+     * To print customer details.
+     */
     override fun toString(): String {
         return "CustomerInfo : ${this.name},$stockAccount"
     }
 
+    /**
+     * Function to override hashCode function.
+     */
+    override fun hashCode(): Int {
+        return {name+stockAccount.hashCode()}.hashCode()
+    }
 }
