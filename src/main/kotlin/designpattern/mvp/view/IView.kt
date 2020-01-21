3dc0748c7d6a6c4709d0fbaea3fe3631cd3690bf
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-21 10:53:51 +0530

    [soundarya]Refactor. Added file comment to IView.kt

--- src/main/kotlin/designpattern/mvp/view/IView.kt
+++ src/main/kotlin/designpattern/mvp/view/IView.kt
@@ -1,9 +1,16 @@
 /**
  * Design Pattern
- * @purpose
+ * @purpose To implement MVP design pattern.
+ * @file IView.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 20/01/2020
  */
 package designpattern.mvp.view
 
+/**
+ * View Interface
+ */
 interface IView {
     fun onAddFailure(message : String)
     fun onAddSuccess(message : String)
