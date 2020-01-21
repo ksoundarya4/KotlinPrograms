6b1e5420e64d0a68dafff1a13b5082aea743ea20
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-21 10:53:11 +0530

    [soundarya]Refactor. Added file comment to StudentPresenter.kt

--- src/main/kotlin/designpattern/mvp/presenter/StudentPresenter.kt
+++ src/main/kotlin/designpattern/mvp/presenter/StudentPresenter.kt
@@ -1,3 +1,11 @@
+/**
+ * Design Pattern
+ * @purpose To implement MVP design pattern.
+ * @file StudentPresenter.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 20/01/2020
+ */
 package designpattern.mvp.presenter
 
 import designpattern.mvp.model.IModel
@@ -7,12 +15,30 @@ import java.io.File
 
 class StudentPresenter(val view: IView, val model: IModel) : IPresenter {
 
+    /**
+     * Function to add Student to .json file
+     */
     override fun addStudent(file: File, student: Student) {
-        model.addStudent(student)
+        model.saveStudent(file, student)
+
+        val savedStudent = model.readStudent(file)
+        if(savedStudent.equals(student))
+        view.onAddSuccess("Added Successfully")
+        else view.onAddFailure("Add is Unsuccessful")
     }
 
+    /**
+     * Function to remove student from .json file
+     */
     override fun deleteStudent(file: File, student: Student) {
-        model.removeStudent(student)
+        val removeStudent = model.readStudent(file)
+
+        if (removeStudent.equals(student)) {
+            removeStudent.name = ""
+            removeStudent.rollNumber = ""
+            model.saveStudent(file, removeStudent)
+            view.onDeleteSuccess("Deleted Successfully")
+        } else view.onDeleteFailure("Delete Unsuccessful")
     }
 
 }
