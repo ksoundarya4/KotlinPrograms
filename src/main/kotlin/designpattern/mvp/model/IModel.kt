4b861ea8c55c6869ff79d54e097ddc92b2dd17d4
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-21 10:51:52 +0530

    [soundarya]Refactor. Added file comments to IModel.kt

--- src/main/kotlin/designpattern/mvp/model/IModel.kt
+++ src/main/kotlin/designpattern/mvp/model/IModel.kt
@@ -1,7 +1,17 @@
-package designpattern.mvp.model
+/**
+ * Design Pattern
+ * @purpose To implement MVP design pattern.
+ * @file IModel.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 20/01/2020
+ */package designpattern.mvp.model
 
 import java.io.File
 
+/**
+ * Model Interface
+ */
 interface IModel {
 
     fun readStudent(file : File) : Student
--- src/main/kotlin/designpattern/mvp/model/Student.kt
+++ src/main/kotlin/designpattern/mvp/model/Student.kt
@@ -1,7 +1,7 @@
 /**
  * Design Pattern
  * @purpose To implement MVP design pattern.
- * @file ksoundarya4
+ * @file Student.kt
  * @author ksoundarya4
  * @version 1.0
  * @since 20/01/2020
