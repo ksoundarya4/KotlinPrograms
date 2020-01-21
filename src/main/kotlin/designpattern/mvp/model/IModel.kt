61c53cf276daed8a9aecd087d3a0a8a78caed6ca
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-21 10:12:02 +0530

    [soundarya]Fix. Fixed IModel.kt

--- src/main/kotlin/designpattern/mvp/model/IModel.kt
+++ src/main/kotlin/designpattern/mvp/model/IModel.kt
@@ -4,8 +4,7 @@ import java.io.File
 
 interface IModel {
 
-    fun readStudent(file : File) : Students
+    fun readStudent(file : File) : Student
     fun saveStudent(file : File , student : Student)
-    fun addStudent(student : Student)
-    fun removeStudent(student: Student)
+
 }
