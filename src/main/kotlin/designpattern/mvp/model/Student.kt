c9db491b44a0afe8741045d29bee8743600f2d07
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-21 10:12:49 +0530

    [soundarya]Refactor. Added equals,toString and hashCode functions to Student.kt

--- src/main/kotlin/designpattern/mvp/model/Student.kt
+++ src/main/kotlin/designpattern/mvp/model/Student.kt
@@ -8,4 +8,30 @@
  */
 package designpattern.mvp.model
 
-data class Student(val name : String, val rollNumber : String)
+data class Student(var name : String, var rollNumber : String){
+
+    /**
+     * Function to override equals function.
+     */
+    override fun equals(other: Any?): Boolean {
+        if(other is Student){
+            if(other.name == this.name &&
+                    other.rollNumber == this.rollNumber) return true
+        }
+        return false
+    }
+
+    /**
+     * Function to override toString function
+     */
+    override fun toString() : String{
+        return "Student : $name,$rollNumber"
+    }
+
+    /**
+     * Function to override hashCode function
+     */
+    override fun hashCode() :Int {
+        return {name+rollNumber}.hashCode()
+    }
+}
